fba91d22f6b4e9fb98124ba38ae781a2adc6ba5b
Oluyemi Paul <oluola96@gmail.com>
2024-06-24 02:35:00 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,34 +4,10 @@ import Enterprise from "./components/Enterprise";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
-<<<<<<< HEAD
-import { Select } from "./components/Select";
-import SignUp from "./components/SignUp";
-import Testing from "./components/Testing";
-import Test2 from "./components/Test2";
-import UpworkAndBiz from "./components/ui/Home/UpworkAndBiz";
-=======
->>>>>>> 230f7677707c6f33b67c846a37c5564a4bf5628a
-
 import SignUp from "./components/SignUp";
 
 export default function app() {
   return (
-<<<<<<< HEAD
-    <div >
-      <NavBar />
-      <Home />
-      {/* <Test2/> */}
-      <UpworkAndBiz/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/enterprise" element={<Enterprise />} />
-      </Routes>
-      <Login />
-      {/* <Testing /> */}
-      {/* <Select /> */}
-    </div>
-=======
     <Router>
       <div>
         <NavBar />
@@ -41,6 +17,5 @@ export default function app() {
         </Routes>
       </div>
     </Router>
->>>>>>> 230f7677707c6f33b67c846a37c5564a4bf5628a
   );
 }
